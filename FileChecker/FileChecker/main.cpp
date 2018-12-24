e4234868975bc163989ff67bf41d4cc88bb1d100
vika-08 <vikakalinina13@mail.ru>
2018-12-25 01:20:06 +0300

    fileFinish1

--- FileChecker/FileChecker/main.cpp
+++ FileChecker/FileChecker/main.cpp
@@ -14,18 +14,35 @@ bool check(const string& inp) {
 	}
 	return false;
 }
+
 int main() {
 	setlocale(LC_ALL, "");
 	ifstream in("in.txt");
 	string line;//предложение
 	stringstream ss;//строковый поток
-	while (getline(in, line)) {//считываем по предложению
-							   //если строка начинается с тире
-		if (check(line)) {
-			ss << line << '\n';
+
+	try
+	{
+
+		if (in.is_open())
+		{
+			while (getline(in, line)) {//считываем по предложению
+									   //если строка начинается с тире
+				if (check(line)) {
+					ss << line << '\n';
+				}
+			}
 		}
+		else throw exception("Не удалось открыть файл!");
+
+		cout << ss.str();
 	}
-	cout << ss.str();
+
+	catch (const exception &err)
+	{
+		cerr << err.what() << endl;
+	}
+
 	in.close();//закрываем файл			
 
 	system("pause>>void");
